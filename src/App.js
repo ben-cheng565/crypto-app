837bf30abec208310c697705da72d45cf27d7981
Ben Cheng <xche565@aucklanduni.ac.nz>
2022-03-15 15:49:00 +1300

    refactor: Define the structure of main page.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import "./App.css";
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <div className="navbar"></div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  );
 }
