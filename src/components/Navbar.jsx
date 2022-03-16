c761faea1d2f23ea7a1f523fd8c2f813ba42232e
Ben Cheng <xche565@aucklanduni.ac.nz>
2022-03-16 21:59:49 +1300

    feat: Add system menus.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Avatar, Typography, Button } from "antd";
+import { Avatar, Typography, Button, Menu } from "antd";
 
 import icon from "../images/logo.png";
+import {
+  BulbOutlined,
+  FundOutlined,
+  HomeOutlined,
+  MoneyCollectOutlined,
+} from "@ant-design/icons";
 
 export default function Navbar() {
   return (
     <div className="nav-container">
       <div className="logo-container">
         <Avatar src={icon} size="small" />
+
         <Typography.Title level={3} className="logo">
           <Link to="/">Cryptor</Link>
         </Typography.Title>
-        <Button></Button>
+
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
       </div>
     </div>
   );
