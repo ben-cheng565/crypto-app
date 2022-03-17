c1562df0b682aff9a322fe62daea15f826e66841
Ben Cheng <xche565@aucklanduni.ac.nz>
2022-03-17 20:22:17 +1300

    feat: Create components for each menu.

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
 export { default as Navbar } from "./Navbar";
+export { default as Homepage } from "./Homepage";
+export { default as Cryptos } from "./Cryptos";
+export { default as Exchanges } from "./Exchanges";
+export { default as CryptoDetail } from "./CryptoDetail";
+export { default as News } from "./News";
