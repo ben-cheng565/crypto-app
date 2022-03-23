92dd7babd083f655efeccf807ffc29fee81acc0e
Ben Cheng <xche565@aucklanduni.ac.nz>
2022-03-23 19:46:24 +1300

    feat: Fetch cryptos statistic data from api and display them.

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -9,6 +9,11 @@ const { Title } = Typography;
 
 export default function Homepage() {
   const { data, isFetching } = useGetCryptosQuery();
+  const globalStats = data?.data?.stats;
+
+  if (isFetching) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
@@ -17,11 +22,23 @@ export default function Homepage() {
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value={5}></Statistic>
-          <Statistic title="Total Exchanges" value={5}></Statistic>
-          <Statistic title="Total Market Cap" value={5}></Statistic>
-          <Statistic title="Total 24h Volume" value={5}></Statistic>
-          <Statistic title="Total Markets" value={5}></Statistic>
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
+          <Statistic
+            title="Total Market Cap"
+            value={millify(globalStats.totalMarketCap)}
+          />
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalStats.total24hVolume)}
+          />
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          />
         </Col>
       </Row>
     </>
