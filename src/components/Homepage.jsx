f8ad7a5ea31dfa65e85525a0ea40da0a154e653f
Ben Cheng <xche565@aucklanduni.ac.nz>
2022-03-23 19:02:12 +1300

    feat: Design home page layout.

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Typography, Row, Col, Statistic } from "antd";
+
+const { Title } = Typography;
 
 export default function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value={5}></Statistic>
+          <Statistic title="Total Exchanges" value={5}></Statistic>
+          <Statistic title="Total Market Cap" value={5}></Statistic>
+          <Statistic title="Total 24h Volume" value={5}></Statistic>
+          <Statistic title="Total Markets" value={5}></Statistic>
+        </Col>
+      </Row>
+    </>
+  );
 }
