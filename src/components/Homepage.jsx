2972a6cd8cbad76f99bfc232e6ab0cf0dda9d8e3
Ben Cheng <xche565@aucklanduni.ac.nz>
2022-03-23 19:29:03 +1300

    feat: Create api endpoints for redux hooks.

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,9 +3,13 @@ import millify from "millify";
 import { Link } from "react-router-dom";
 import { Typography, Row, Col, Statistic } from "antd";
 
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
 const { Title } = Typography;
 
 export default function Homepage() {
+  const { data, isFetching } = useGetCryptosQuery();
+
   return (
     <>
       <Title level={2} className="heading">
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  "X-RapidAPI-Key": "02f8d474admshb2cbac47d6ec706p1e569fjsnde7fe0e96dc3",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
