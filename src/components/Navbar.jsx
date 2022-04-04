4d783874023b5096ebcb29e914f58cd4f4d4776e
Ben Cheng <xche565@aucklanduni.ac.nz>
2022-04-05 10:54:36 +1200

    feat: Hide menus for small screen.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Avatar, Typography, Button, Menu } from "antd";
 
@@ -7,10 +7,30 @@ import {
   BulbOutlined,
   FundOutlined,
   HomeOutlined,
+  MenuOutlined,
   MoneyCollectOutlined,
 } from "@ant-design/icons";
 
 export default function Navbar() {
+  const [menuVisible, setMenuVisible] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize < 768) {
+      setMenuVisible(false);
+    } else {
+      setMenuVisible(true);
+    }
+  }, [screenSize]);
+
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -19,21 +39,32 @@ export default function Navbar() {
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptor</Link>
         </Typography.Title>
+
+        <Button
+          type="link"
+          className="menu-control-container"
+          onClick={() => setMenuVisible(!menuVisible)}
+        >
+          <MenuOutlined />
+        </Button>
       </div>
-      <Menu theme="dark">
-        <Menu.Item icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item icon={<FundOutlined />}>
-          <Link to="/cryptos">Cryptocurrencies</Link>
-        </Menu.Item>
-        <Menu.Item icon={<MoneyCollectOutlined />}>
-          <Link to="/exchanges">Exchanges</Link>
-        </Menu.Item>
-        <Menu.Item icon={<BulbOutlined />}>
-          <Link to="/news">News</Link>
-        </Menu.Item>
-      </Menu>
+
+      {menuVisible && (
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptos">Cryptos</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      )}
     </div>
   );
 }
