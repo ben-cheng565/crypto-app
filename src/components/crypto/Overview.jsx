a9295293060b22442eacb7194391f4255aa0b498
Ben Cheng <xche565@aucklanduni.ac.nz>
2022-04-05 15:42:01 +1200

    feat: Create overview component to contain crypto stats and linechart

---
+++ src/components/crypto/Overview.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { millify } from "millify";
+import { Row, Col } from "antd";
+
+import {
+  MoneyCollectOutlined,
+  DollarCircleOutlined,
+  FundOutlined,
+  ExclamationCircleOutlined,
+  StopOutlined,
+  TrophyOutlined,
+  CheckOutlined,
+  NumberOutlined,
+  ThunderboltOutlined,
+} from "@ant-design/icons";
+
+import { LineChart, CoinStat } from "..";
+
+export default function Overview({ cryptoDetails, coinHistory }) {
+  const stats = [
+    {
+      title: "Price to USD",
+      value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`,
+      icon: <DollarCircleOutlined />,
+    },
+    { title: "Rank", value: cryptoDetails?.rank, icon: <NumberOutlined /> },
+    {
+      title: "24h Volume",
+      value: `$ ${
+        cryptoDetails["24hVolume"] && millify(cryptoDetails["24hVolume"])
+      }`,
+      icon: <ThunderboltOutlined />,
+    },
+    {
+      title: "Market Cap",
+      value: `$ ${
+        cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)
+      }`,
+      icon: <DollarCircleOutlined />,
+    },
+    {
+      title: "All-time-high",
+      value: `$ ${
+        cryptoDetails?.allTimeHigh?.price &&
+        millify(cryptoDetails?.allTimeHigh?.price)
+      }`,
+      icon: <TrophyOutlined />,
+    },
+  ];
+
+  const genericStats = [
+    {
+      title: "Number Of Markets",
+      value: cryptoDetails?.numberOfMarkets,
+      icon: <FundOutlined />,
+    },
+    {
+      title: "Number Of Exchanges",
+      value: cryptoDetails?.numberOfExchanges,
+      icon: <MoneyCollectOutlined />,
+    },
+    {
+      title: "Aprroved Supply",
+      value: cryptoDetails?.supply?.confirmed ? (
+        <CheckOutlined />
+      ) : (
+        <StopOutlined />
+      ),
+      icon: <ExclamationCircleOutlined />,
+    },
+    {
+      title: "Total Supply",
+      value: `$ ${
+        cryptoDetails?.supply?.total && millify(cryptoDetails?.supply?.total)
+      }`,
+      icon: <ExclamationCircleOutlined />,
+    },
+    {
+      title: "Circulating Supply",
+      value: `$ ${
+        cryptoDetails?.supply?.circulating &&
+        millify(cryptoDetails?.supply?.circulating)
+      }`,
+      icon: <ExclamationCircleOutlined />,
+    },
+  ];
+
+  return (
+    <>
+      <Row className="stats-container">
+        <Col xs={24} sm={12} className="coin-value-statistics">
+          {stats.map((stat) => (
+            <CoinStat key={stat.title} {...stat} />
+          ))}
+        </Col>
+
+        <Col xs={24} sm={12} className="coin-value-statistics">
+          {genericStats.map((stat) => (
+            <CoinStat key={stat.title} {...stat} />
+          ))}
+        </Col>
+      </Row>
+
+      <LineChart
+        coinHistory={coinHistory}
+        currentPrice={millify(cryptoDetails.price)}
+        coinName={cryptoDetails.name}
+      />
+    </>
+  );
+}
