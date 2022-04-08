391d03b57c7b1b70eba6e041b610168afe20d859
Ben Cheng <xche565@aucklanduni.ac.nz>
2022-04-08 15:54:13 +1200

    build: install babel packages to load js files in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,24 @@ const path = require("path");
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
