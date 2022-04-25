65eaa38f6e62af5d8f9095631fbd11b6991a4625
Ben Cheng <xche565@aucklanduni.ac.nz>
2022-04-25 20:43:36 +1200

    refactor: redesign the page frame of the app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import { Layout, Typography, Space } from "antd";
+import { Routes, Route } from "react-router-dom";
+import { Layout, Typography } from "antd";
 
 import { Navbar, Homepage, Cryptos, CryptoDetail, News } from "./components";
 import "./sass/app.scss";
 
+const { Header, Footer, Content } = Layout;
+
 export default function App() {
   return (
-    <div className="app">
-      <div className="navbar">
+    <Layout>
+      <Header>
         <Navbar />
-      </div>
-      <div className="main">
-        <Layout>
-          <div className="routes">
-            <Routes>
-              <Route exact path="/" element={<Homepage />}></Route>
-              <Route path="/cryptos" element={<Cryptos />}></Route>
-              <Route path="/crypto/:coinId" element={<CryptoDetail />}></Route>
-              <Route path="/news" element={<News />}></Route>
-            </Routes>
-          </div>
-        </Layout>
+      </Header>
 
-        <div className="footer">
-          <Typography.Title
-            level={5}
-            style={{ color: "white", textAlign: "center" }}
-          >
-            Cryptor @2022 - All rights reserved
-          </Typography.Title>
-        </div>
-      </div>
-    </div>
+      <Content className="main-container">
+        <Routes>
+          <Route exact path="/" element={<Homepage />}></Route>
+          <Route path="/cryptos" element={<Cryptos />}></Route>
+          <Route path="/crypto/:coinId" element={<CryptoDetail />}></Route>
+          <Route path="/news" element={<News />}></Route>
+        </Routes>
+      </Content>
+
+      <Footer>
+        <Typography.Text>Cryptor @2022 - All rights reserved</Typography.Text>
+      </Footer>
+    </Layout>
   );
 }
